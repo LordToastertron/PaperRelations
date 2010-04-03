37274b7e703abe4603d7306534c35155e8178b2a
Alexander Honkala <Toaster.Sunshine@gmail.com>
2010-04-03 16:43:27 -0400

    Add empty PLOSPaper class

---
+++ Papers/PLOSPaper.py
@@ -0,0 +1,12 @@
+Python 2.6.2 (r262:71605, Apr 14 2009, 22:40:02) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 2.6.2      
+>>> 
