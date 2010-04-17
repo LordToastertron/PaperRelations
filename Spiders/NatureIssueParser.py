8d320e600ae7eb68e1bc7a0ba32ea3548916782b
Nathan Dotz <nathan.dotz@gmail.com>
2010-04-17 19:22:52 -0400

    pull links to articles from issue pages

--- Spiders/NatureIssueParser.py
+++ Spiders/NatureIssueParser.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 #
-# NatureIndexParser.py
+# NatureVolumeParser.py
 # created: nathan dotz - nathan (dot) dotz (at) gmail (dot) com
 # license: GNU GPL3 - see LICENSE file for details
-# Grabs the set of links from an index page of Nature-type articles
-# to be used to spider for other citations
+# Grabs the set of links from an volume page of Nature-type articles
+# to be used to spider for citation files
 
 from Parser import Parser
 import re
 
-class NatureIndexParser(Parser):
+class NatureIssueParser(Parser):
 
     def start_a(self,attrs):
         for prop, value in attrs:
-            if prop == "href" and re.search("^/nbt/journal/v[0-9]", value) != None:
+            if prop == "href" and re.search("^/nbt/journal/v[0-9].*full.*", value) != None:
                 self.links.append(value)
-
--- Spiders/NatureSpider.py
+++ Spiders/NatureSpider.py
@@ -5,15 +5,17 @@
 # license: GNU GPL3 - see LICENSE file for details
 
 from NatureIndexParser import NatureIndexParser
+from NatureIssueParser import NatureIssueParser
 import urllib
 
 class NatureSpider:
 
     def __init__(self):
         self.idxparser = NatureIndexParser()
+        self.issues = []
 
     def readIndex(self, resourceName):
-        "Reads a resource to parse by index parser. This should be a "
+        "Reads a resource to parse for links to issues. This should be a "
         "filename or url. urls are assumed to start with 'http'"
         if(resourceName[:4] == 'http'):
             f = urllib.urlopen(resourceName)
@@ -21,7 +23,27 @@ class NatureSpider:
             f = open(resourceName)
         self.idxparser.parse(f.read())
 
+    def readIssue(self, resourceName):
+        "Read a resource to fetch all links to article texts"
+        if(resourceName[:4] == 'http'):
+            f = urllib.urlopen(resourceName)
+        else:
+            f = open(resourceName)
+        i = NatureIssueParser()
+        i.parse(f.read())
+        self.issues.append(i)
+
+    def idxlinks(self):
+        return self.idxparser.links
+
 if __name__ == '__main__':
     n = NatureSpider()
     n.readIndex('http://www.nature.com/nbt/archive/index.html')
-    print n.idxparser.links
+    i = 0
+    for l in n.idxlinks():
+        if i > 10:
+            break
+        n.readIssue('http://www.nature.com'+l)
+        i += 1
+    for i in n.issues:
+        print i.links
