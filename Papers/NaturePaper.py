61734e91aa66f68824208645f0ffb0654cefcafc
Nathan Dotz <nathan.dotz@gmail.com>
2010-09-06 10:53:04 -0400

    Clean up testing of .ris files for NaturePaper types.

--- Papers/NaturePaper.py
+++ Papers/NaturePaper.py
@@ -17,6 +17,9 @@ class NaturePaper(Paper):
         Paper.__init__(self)
         self.citFile = citFile
         self.refFile = refFile
+        if self.citFile and self.refFile:
+            self.loadCitFile(self.citFile)
+            self.loadRefFile(self.refFile)
 
     def parseCitation(self, splitLinesFromCitation):
         "Takes a list of split lines from a citation file, and iterates "
@@ -27,6 +30,7 @@ class NaturePaper(Paper):
             try:
                 head, info = line.split('-',1)
             except ValueError:
+                
                 continue
             #print "split",head,"and",info
             if head.strip() == "TI": self.Title = info.strip()
@@ -42,6 +46,8 @@ class NaturePaper(Paper):
         "Takes a filename, opens file, then sends the split "
         "lines to parseCitation to load the current Paper's citation"
         handle = open(filename, 'r')
+        if not self.citFile == filename:
+            self.citFile = filename
         lines = handle.read()
         lines = lines.split("\n")
         self.parseCitation(lines)
@@ -50,6 +56,8 @@ class NaturePaper(Paper):
         "Takes a filename, opens file, splits on blank lines,"
         " then splits again to create new objects via parseCitation"
         handle = open(filename, 'r')
+        if not self.refFile == filename:
+            self.refFile = filename
         lines = handle.read()
         lines = lines.split("\n\n")
         for line in lines:
@@ -59,12 +67,7 @@ class NaturePaper(Paper):
 
 
 if __name__ == '__main__':
-    print "Testing NaturePaper:\n"
-    print 'Instantiating NaturePaper'
-    n = NaturePaper()
-    print 'Successfully instantiated n as NaturePaper'
-    print 'Loading citation file'
-    n.loadCitFile("/home/nate/workspace/PaperRelations/nbt.1618.ris")
-    print 'Citation file loaded'
-    print 'Loading References file'
-    n.loadRefFile("/home/nate/workspace/PaperRelations/nbt.1618refs.ris")
+    filename = raw_input("Please enter a filename to parse:\n")
+    ref = filename.split('.ris')[0]
+    np = NaturePaper(ref+'.ris',ref+'refs.ris')
+    print np.Title, ' - ', np.Authors
